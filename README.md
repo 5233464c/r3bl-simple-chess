c906a87866a8aae3d43f2c634255aa007c64c459
Felix Lueth <felix.lueth@t-online.de>
2020-05-16 14:58:58 +0200

    Move figures with user input
    * Update README.md
    * Ignore node_modules folder
    * Install prompt-sync

--- README.md
+++ README.md
@@ -9,9 +9,9 @@
 * 32 Figuren ✔️
 * 1 Spielbrett mit 8x8 Feldern ✔️
 * Figuren können sich auf dem Spielfeld bewegen ✔️
-* 2 Spieler können Figuren kontrollieren
-* Jeder Spieler kann nur seine Figuren kontrollieren
-* Das Spiel ist rundenbasiert
+* 1 Spieler kann seine Figuren kontrollieren ✔️
+* 1 Spieler kann nur seine zugehörigen Figuren bewegen
+* Das Spiel ist rundenbasiert ✔️
 
 
 ## Projekt einrichtne
@@ -21,55 +21,3 @@
 
 ## Software
 * NVM (https://github.com/nvm-sh/nvm)
-
-
-## Notizen
-/**
- * Spielzug von a1 nach a2 
- * 
- * input = 'a2';
- * ist a2 frei?
- * 
- * Boardzustand prüfen:
- * 
- * CollisionDetection(board)
- * function: isValid(position, figure) {
- *      position // a2 methode
- * 
- *       if( board.a.2) {
- *          ja: board.a.2 = true
- *          ja: figure.move(a2)
- *          ja: board.a.1 = false// alter zustand auf false ...
- *          return true;
- *       }    
- *     nein: new InvalidMoveException()
- 
- *      
- * }
- * 
-
- *     
- *   
- * 
- * figure.move(a2) 
- * 
- * 
- */
-
-
-// move a2 to a5
-
-// a2 = false
-// a5 = true
-
-// initial figure position a1
-
-// figure.move(a1, a2)
-// 6 Unterschiedliche figuren 
-
-// 8 Bauern
-// 2 Tuerme
-// 2 Laeufer
-// 2 Pferde
-// 1 Dame 
-// 1 Koenig 
--- index.js
+++ index.js
@@ -1,24 +1,46 @@
 let board = require("./src/board.js");
 let Figure = require("./src/figure.js");
 let CollisionDetection = require("./src/collision.js");
+
+let prompt = require('prompt-sync')();
+
 console.log('RBFL SIMPLE CHESS');
 
 let loop = true
 
+let figures = {
+    f1: new Figure('a1'),
+    f2: new Figure('a2'),
+
+    get: (selected) => {
+        if (figures.f1.position == selected) {
+            return figures.f1;
+        }
+        if (figures.f2.position == selected) {
+            return figures.f1;
+        }
+        throw new Error('InvalidSelectedFigureException')
+    }
+}
+let collision = new CollisionDetection(board);
+
 while (loop) {
-    let input = 'a3';
-    let figure1 = new Figure('a1');
-    let collision = new CollisionDetection(board);
+    let selectedFigure = prompt('Spielfigur? ');
+    let figure = null;
+
     try {
-        collision.isValid(input, figure1);
+        figure = figures.get(selectedFigure);
     } catch (e) {
         console.error(e.name + ': ' + e.message)
-        loop = false
+        continue;
     }
 
+    let moveFigureTo = prompt('Bewege Spielfigure nach: ')
+    try {
+        collision.isValid(moveFigureTo, figure);
+    } catch (e) {
+        console.error(e.name + ': ' + e.message)
+        process.exit(0);
+    }
+    console.log(board.Board.a);
 }
-// Benutzereingabe zur Veränderung der Position 
-// Gameloop mit wiederholter Benutzereingabe
-// Figur wird von A1 nach A2 bewegt
-// Input 1: Wähle Figur(enthält Ausgangsposition)
-// Input 2: Bewege Figur(zur Endposition)
