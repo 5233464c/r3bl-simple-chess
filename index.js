a5699a113afdff043a627d51efde769d852f82e8
Felix Lueth <felix.lueth@t-online.de>
2020-05-16 14:05:45 +0200

    Add simple game loop

--- index.js
+++ index.js
@@ -3,19 +3,22 @@ let Figure = require("./src/figure.js");
 let CollisionDetection = require("./src/collision.js");
 console.log('RBFL SIMPLE CHESS');
 
+let loop = true
 
-let input = 'a3';
-let figure1 = new Figure('a1');
-let collision = new CollisionDetection(board);
-console.log(figure1.position)
-console.log(board.Board.a)
-try {
-    collision.isValid(input, figure1);
-} catch (e) {
-    console.error(e.name + ': ' + e.message)
+while (loop) {
+    let input = 'a3';
+    let figure1 = new Figure('a1');
+    let collision = new CollisionDetection(board);
+    try {
+        collision.isValid(input, figure1);
+    } catch (e) {
+        console.error(e.name + ': ' + e.message)
+        loop = false
+    }
+
 }
-console.log(figure1.position)
-
-console.log(board.Board.a)
-
-
+// Benutzereingabe zur Veränderung der Position 
+// Gameloop mit wiederholter Benutzereingabe
+// Figur wird von A1 nach A2 bewegt
+// Input 1: Wähle Figur(enthält Ausgangsposition)
+// Input 2: Bewege Figur(zur Endposition)
